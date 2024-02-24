bbb5f48d9016f86a48ba4428d74afb0c633a86eb
Mohamed Riyas. Ashraf Ali <riyassucces@gmail.com>
2024-02-24 19:58:15 +0530

    tail wind implement

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import TitleCard from "./Compnents/TitleCard";
 function App() {
   return (
-    <>
+    <div className="bg-[#141a17ff]">
       <TitleCard />
-    </>
+    </div>
   );
 }
 
--- src/Compnents/TitleCard.jsx
+++ src/Compnents/TitleCard.jsx
@@ -3,8 +3,9 @@ import logo from "../assets/investment-calculator-logo.png";
 
 function TitleCard() {
   return (
-    <div>
-      <img src={logo} />
+    <div className=" flex justify-center items-center w-screen h-screen flex-col">
+      <img src={logo} className="w-20 h-20" />
+      <p className="pt-4 font-bold text-custom-20">Investment Calculator</p>
     </div>
   );
 }
